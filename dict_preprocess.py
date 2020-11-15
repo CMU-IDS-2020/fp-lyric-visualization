d913aa669fbcd632b42f3fb7a7f592c4b2e8ce18
Paulina Davison <pldaviso@andrew.cmu.edu>
2020-11-14 23:16:01 -0800

    code for making a dictionary dataframe with counts and word indixes (happily, sorted by the word's first appearance)

--- dict_preprocess.py
+++ dict_preprocess.py
@@ -29,15 +29,49 @@ song_name = "Just the Two of Us"
 # Read in the lyrics dataframe
 lyrics_df = pd.read_csv(artist_name + " " + song_name + " lyrics.csv")
 
-# Make a dictionary Panda dataframe of the words that are in the song
-# With the count of how many times the word appears in the song
-# And the word indexes from the beginning
-# (including all the chorus duplicates and also not including the chorus duplicates)
-dict_df = pd.DataFrame(columns = ["word_can_search", "count_all", "count_with_first_chorus", "count_with_no_chorus"])
+# Make four dictionaries of the words that are in the song, all indexed by word (equivalent of word_can_search)
+# 1. Count of all appearances of the word (including all choruses)
+all_chorus_count = {}
+# 2. Count of all appearances of the word (including only the first chorus)
+first_chorus_count = {}
+# 3. Count of all appearances of the word (not including any of the choruses)
+no_chorus_count = {}
+# 4. List of word_index_in_song values for all the appearances of the word
+word_indexes = {}
+past_first_chorus = False
 for index, row in lyrics_df.iterrows():
-	if (row[Lyrics_Columns.STANZA_DESCRIPTION.value] == "Chorus"):
-		print(row)
-		print("=======")
+	# for readability, get row values ahead
+	word = row[Lyrics_Columns.WORD_CAN_SEARCH.value]
+	word_index_in_song = row[Lyrics_Columns.WORD_INDEX_IN_SONG.value]
+	stanza_description = row[Lyrics_Columns.STANZA_DESCRIPTION.value]
+	# increment counts
+	if (stanza_description == "Chorus") and not past_first_chorus:
+		# the word is in the first chorus
+		# increment the count_with_all_chorus and the count_with_first_chorus for this word
+		all_chorus_count[word] = all_chorus_count.get(word, 0) + 1
+		first_chorus_count[word] = first_chorus_count.get(word, 0) + 1
+		# this is the first chorus, so change the value of past_first_chorus
+		past_first_chorus = True
+	elif (stanza_description == "Chorus"):
+		# the word is in a chorus that is not the first chorus
+		# increment the count_with_all_chorus for this word
+		all_chorus_count[word] = all_chorus_count.get(word, 0) + 1
+	else:
+		# the word is not in chorus
+		# increment the count_with_all_chorus, count_with_first_chorus, and count_with_no_chorus for this word
+		all_chorus_count[word] = all_chorus_count.get(word, 0) + 1
+		first_chorus_count[word] = first_chorus_count.get(word, 0) + 1
+		no_chorus_count[word] = no_chorus_count.get(word, 0) + 1
+	# add this word's index to the word_indexes dictionary
+	word_indexes.setdefault(word, []).append(word_index_in_song)
+
+# Make a dictionary Panda dataframe from the above dictionaries
+dict_columns = ["word_can_search", "count_with_all_chorus", "count_with_first_chorus", "count_with_no_chorus", "list_of_indexes"]
+dict_df = pd.DataFrame(columns = dict_columns)
+for word in word_indexes.keys():
+	word_df = pd.DataFrame([[word, all_chorus_count.get(word, 0), first_chorus_count.get(word, 0), 
+								no_chorus_count.get(word, 0), word_indexes.get(word)]], columns = dict_columns)
+	dict_df = dict_df.append(word_df, ignore_index = True)
 
 # Use the TextBlob Library for each word
 # Add the results to the dictionary Panda Dataframe
